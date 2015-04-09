f12ce293f5d97b15d9859bf788561a7d4ba60d77
Intrainos <intrainos@hotmail.com>
2015-04-09 20:02:01 +0900

    Dev. Progress - 1 %

---
+++ src/main/java/io/github/intrainos/samsungltool/Main.java
@@ -0,0 +1,43 @@
+package io.github.intrainos.samsungltool;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class Main extends Activity {
+    public static final int MENU1 = Menu.FIRST +0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        menu.add(menu.NONE, MENU1, Menu.NONE, "Homepage");
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ src/main/java/io/github/intrainos/samsungltool/TouchLight.java
@@ -0,0 +1,14 @@
+package io.github.intrainos.samsungltool;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TouchLight extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_touchlight);
+    }
+}
